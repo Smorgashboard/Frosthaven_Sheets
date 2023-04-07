eeb5dfd03cbfe1682813c4e9b36157df4da38803
Smorgashboard <100943947+Smorgashboard@users.noreply.github.com>
2023-04-07 18:19:22 -0400

    Final2.0
    Final final with retirementgoals

--- NotesView.swift
+++ NotesView.swift
@@ -33,74 +33,112 @@ struct NoteView: View {
     }
     
     var body: some View {
+        
+        let retirement = characters.first!.retirementGoal ?? "None"
+        let retirementText = retirementGoals[retirement] ?? ("","","")
+        
         ZStack{
             mainBackgroundGradient.ignoresSafeArea()
-        VStack {
-            VStack {
-                Text("Notes")
-                    .font(.title)
-                    .padding(.vertical)
-                    .foregroundColor(titleColor)
-                    .bold()
-                Divider()
-                    .bold()
-                    .foregroundColor(titleColor)
-                TextEditor(text: Binding(
-                    get: { characters.first?.notes as? String ?? "" },
-                    set: { newValue in
-                        characters.first?.notes = newValue
-                        saveContext()
-                    }
-                ))
-                .padding()
-            }
-            .background(tileColor)
-            .cornerRadius(10)
-            .padding()
-            .shadow(color: borderColor.opacity(0.3), radius: 5, x: 5, y: 5)
-            
-            VStack {
-                Text("Counters")
-                    .font(.title)
-                    .bold()
-                    .padding(.vertical)
-                    .foregroundColor(titleColor)
-                Divider()
-                    .bold()
-                    .foregroundColor(titleColor)
-                ForEach(0..<4) { index in
-                    HStack {
-                        TextField("Change Me", text: Binding(
-                            get: { characters.first?.value(forKey: "counter\(index + 1)Name") as? String ?? "" },
+            ScrollView{
+                VStack {
+                    VStack {
+                        Text("Notes")
+                            .font(.title)
+                            .padding(.vertical)
+                            .foregroundColor(titleColor)
+                            .bold()
+                        Divider()
+                            .bold()
+                            .foregroundColor(titleColor)
+                        TextEditor(text: Binding(
+                            get: { characters.first?.notes as? String ?? "" },
                             set: { newValue in
-                                characters.first?.setValue(newValue, forKey: "counter\(index + 1)Name")
+                                characters.first?.notes = newValue
                                 saveContext()
                             }
                         ))
-                        .padding(.all)
-                        .background(Color.clear)
-                        
-                        Stepper(value: Binding(
-                            get: { Int16(Int(characters.first?.value(forKey: "counter\(index + 1)") as? Int16 ?? 0)) },
-                            set: { newValue in
-                                characters.first?.setValue(Int16(newValue), forKey: "counter\(index + 1)")
-                                saveContext()
-                            }
-                        ), in: 0...Int16.max) {
-                            Text("\(Int(characters.first?.value(forKey: "counter\(index + 1)") as? Int16 ?? 0))")
-                                .padding(.all)
-                        }.padding(.trailing)
+                        .frame(minHeight: 4 * UIFont.preferredFont(forTextStyle: .body).lineHeight)
+                        .padding()
                     }
+                    .background(tileColor)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: borderColor.opacity(0.3), radius: 5, x: 5, y: 5)
+                    
+                    VStack {
+                        Text("Counters")
+                            .font(.title)
+                            .bold()
+                            .padding(.vertical)
+                            .foregroundColor(titleColor)
+                        Divider()
+                            .bold()
+                            .foregroundColor(titleColor)
+                        ForEach(0..<4) { index in
+                            HStack {
+                                TextField("Change Me", text: Binding(
+                                    get: { characters.first?.value(forKey: "counter\(index + 1)Name") as? String ?? "" },
+                                    set: { newValue in
+                                        characters.first?.setValue(newValue, forKey: "counter\(index + 1)Name")
+                                        saveContext()
+                                    }
+                                ))
+                                .padding(.all)
+                                .background(Color.clear)
+                                
+                                Stepper(value: Binding(
+                                    get: { Int16(Int(characters.first?.value(forKey: "counter\(index + 1)") as? Int16 ?? 0)) },
+                                    set: { newValue in
+                                        characters.first?.setValue(Int16(newValue), forKey: "counter\(index + 1)")
+                                        saveContext()
+                                    }
+                                ), in: 0...Int16.max) {
+                                    Text("\(Int(characters.first?.value(forKey: "counter\(index + 1)") as? Int16 ?? 0))")
+                                        .padding(.all)
+                                }.padding(.trailing)
+                            }
+                        }
+                    }
+                    .background(tileColor)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: borderColor.opacity(0.3), radius: 5, x: 5, y: 5)
+                    VStack{
+                        Text("Retirment Goal")
+                            .font(.title)
+                            .bold()
+                            .padding(.vertical)
+                            .foregroundColor(titleColor)
+                        Divider()
+                            .bold()
+                            .foregroundColor(titleColor)
+                        Text("\(retirement)")
+                            .font(.title3)
+                            .padding(.vertical)
+                            .bold()
+                        Divider()
+                        Text("\(retirementText.0)")
+                            .multilineTextAlignment(.leading)
+                            .padding(.horizontal)
+                            .padding(.vertical)
+                        Text("\(retirementText.1)")
+                            .multilineTextAlignment(.leading)
+                            .padding(.horizontal)
+                            .padding(.vertical)
+                        Text("\(retirementText.2)")
+                            .multilineTextAlignment(.leading)
+                            .padding(.horizontal)
+                            .padding(.vertical)
+                    }
+                    .background(tileColor)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: borderColor.opacity(0.3), radius: 5, x: 5, y: 5)
+                    
                 }
+                .padding()
             }
-            .background(tileColor)
-            .cornerRadius(10)
-            .padding()
-            .shadow(color: borderColor.opacity(0.3), radius: 5, x: 5, y: 5)
         }
-        .padding()
-    }
-        
     }
     
     private func saveContext() {
